de84bf16f51d5238a28d80c06fc7aad7ba52e39b
Your Name <you@example.com>
2024-10-09 01:07:43 +0530

    final

--- app.js
+++ app.js
@@ -27,24 +27,16 @@ app.use(
   }),
 );
 
-// // Set the view engine to Pug
-// app.set('view engine', 'pug');
-
-// // // Set the path of views directory
-// app.set('views', path.join(__dirname, 'views'));
-
-// // // Serve static files from public directory
-
 // // // Set Security HTTP headers
 // // // set the Content Security Policy
-// app.use(
-//   helmet.contentSecurityPolicy({
-//     directives: {
-//       defaultSrc: ["'self'"],
-//       // connectSrc: ["'self'", 'https://natours-api-z82r.onrender.com'],
-//     },
-//   }),
-// );
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      // connectSrc: ["'self'", 'https://natours-api-z82r.onrender.com'],
+    },
+  }),
+);
 
 // // Parse JSON request body and limit its size to 10KB
 app.use(express.json({ limit: '10kb' }));
@@ -62,56 +54,50 @@ app.use(
 // // The cookieParser() middleware is being used to parse cookies from incoming requests.
 app.use(cookieParser());
 
-// // Enable Cross-Origin Resource Sharing (CORS) for all routes
-// // app.use(cors());
 
 // // //Limit requests from the same API to prevent abuse
-// const apiLimiter = rateLimit({
-//   windowMs: 60 * 60 * 1000, // 1 hour
-//   max: 100, // 100 requests per window (per IP)
-//   message: 'Too many requests from this IP, please try again in an hour',
-// });
-// app.use('/api', apiLimiter); // apply the rate limiter to API routes only
+const apiLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 100, // 100 requests per window (per IP)
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', apiLimiter); // apply the rate limiter to API routes only
 
 // // Sanitize request data to prevent NoSQL injection attacks
-// app.use(mongoSanitize());
+app.use(mongoSanitize());
 
 // // Sanitize request data to prevent cross-site scripting (XSS) attacks
-// app.use(xss());
+app.use(xss());
 
 // // Prevent parameter pollution by whitelisting certain query parameters
-// app.use(
-//   hpp({
-//     whitelist: [
-//       'duration',
-//       'ratingsAverage',
-//       'ratingsQuantity',
-//       'maxGroupSize',
-//       'difficulty',
-//       'price',
-//     ],
-//   }),
-// );
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 // // // Log HTTP requests in the console in the "dev" format
-// // // Development logging
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'));
-// }
-// app.use(compression());
-// // practice middleware
-// app.use((req, res, next) => {
-//   console.log(req.cookies);
-//   next();
-// });
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(compression());
+
 
 app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-// app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/bookings', bookingRouter);
 
-// Handle requests that do not match any of the defined routes
+
 app.all('*', (req, res, next) => {
   return next(
     new AppError(`Can't find ${req.originalUrl} on this server.`, 404),
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+dotenv.config({ path: `${__dirname}/config.env` });
+const app = require('./app');
+const port = process.env.PORT || 3000;
+console.log('THIS IS PORT',port);
 process.on('uncaughtException', (err) => {
-  console.log(err.name, err.message);
+  console.log(err , err.name, err.message);
   console.log('UNCAUGHT EXCEPTION ! SHUTTING DOWN ');
   server.close(() => {
     process.exit(1);
   });
 });
-dotenv.config({ path: `${__dirname}/config.env` });
-const app = require('./app');
 
 /**ENVIRONMENT VARIABLES*/
 
@@ -21,13 +24,14 @@ mongoose
     console.log('Connected to MongoDB');
   });
 
-const port = process.env.PORT || 3000;
+
+
 /**START THE SERVER */
 const server = app.listen(port, '127.0.0.1', () => {
   console.log('Server is listening');
 });
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
+  console.log('HA RIYAL ERROR',err,err.name, err.message);
   console.log('UNHANDLED REJECTION ! SHUTTING DOWN ');
   server.close(() => {
     process.exit(1);
